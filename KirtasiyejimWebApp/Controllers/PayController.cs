9cd6052cb71169f4de3e70c98c011b5f01175a01
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-05-16 15:00:40 +0300

    Tamamlandi

--- KirtasiyejimWebApp/Controllers/PayController.cs
+++ KirtasiyejimWebApp/Controllers/PayController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,7 +31,46 @@ namespace KirtasiyejimWebApp.Controllers
             }
             List<ShoppingCart> list = db.ShoppingCarts.Where(x => x.User_ID == User_ID).ToList();
             double total = list.Sum(x => x.Product.Price * x.Quantity);
-            return View();
+            string fiyatstr = total.ToString().Replace(",", ".");
+            string apiurl = "https://localhost:44386/API/Pay?kartno=" + cardnumber + "&sonkullanmaAy=" + reqmonth + "&sonkullanmayil=" + reqyear + "&CVV=" + cvv + "&fiyat=" + fiyatstr;
+            //using System.Net.Http;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(apiurl).Result;
+            var strinResp = response.Content.ReadAsStringAsync();
+            if (strinResp.Result == "\"201\"")
+            {
+                return RedirectToAction("PaymentSuccess");
+            }
+            else if (strinResp.Result == "\"901\"")
+            {
+                ViewBag.message = "Kart Numarası Hatalı Girildi";
+            }
+            else if (strinResp.Result == "\"801\"")
+            {
+                ViewBag.message = "Kart Tarihi Eşleşmesi";
+            }
+            else if (strinResp.Result == "\"701\"")
+            {
+                ViewBag.message = "Banka Mesajı = Kart Tarihi Geçersiz";
+            }
+            else if (strinResp.Result == "\"601\"")
+            {
+                ViewBag.message = "Banka Mesajı =Cvv Hatalı";
+            }
+            else if (strinResp.Result == "\"501\"")
+            {
+                ViewBag.message = "Banka Mesajı = Kart Aktif Değil";
+            }
+            else if (strinResp.Result == "\"401\"")
+            {
+                ViewBag.message = "Banka Mesajı = Kart Bakiyesi yetersiz";
+            }
+            else if (strinResp.Result == "\"301\"")
+            {
+                ViewBag.message = "Banka Mesajı = Bir Hata oluştu";
+            }
+            return View(db.ShoppingCarts.Where(x => x.User_ID == User_ID));
         }
+        
     }
 }
